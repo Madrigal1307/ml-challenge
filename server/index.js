14ad4e1fd632153a79dafcbae46618005da09152
fernanda <ma.fernanda@guay.define>
2023-12-08 23:35:09 -0600

    ML challenge

---
+++ server/index.js
@@ -0,0 +1,54 @@
+// server/index.js
+
+const express = require("express");
+const cors = require('cors');
+const axios = require('axios');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+// Configurar CORS
+app.use(cors());
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hola desde el servidor!" });
+});
+
+app.get("/api/items/:query", async (req, res) => {
+    try {
+        // Obtener el valor de la URL
+        const { query } = req.params;
+        // URL de la API externa con el valor proporcionado
+        const apiUrl = `https://api.mercadolibre.com/sites/MLA/search?q=${query}`;
+        // Realizar la solicitud utilizando axios
+        const response = await axios.get(apiUrl);
+        // Enviar la respuesta de la API externa como respuesta de tu servidor
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error al realizar la solicitud a la API externa:', error);
+        res.status(500).json({ error: 'Error al consultar la API externa' });
+    }
+});
+
+app.get("/api/items/:id", async (req, res) => {
+    try {
+        // Obtener el valor de la URL
+        const { id } = req.params;
+        // URL de la API externa con el valor proporcionado
+        const apiUrl = `https://api.mercadolibre.com/items/:${id}`;
+        const apiUrl2 = `https://api.mercadolibre.com/items/:${id}/description`;
+        // Realizar la solicitud utilizando axios
+        const response = await axios.get(apiUrl);
+        const response2 = await axios.get(apiUrl2);
+        // Unir los datos
+        const resultado = { ...response.data, ...response2.data };
+        // Enviar la respuesta de la API externa como respuesta de tu servidor
+        res.json(resultado);
+    } catch (error) {
+        console.error('Error al realizar la solicitud a la API externa:', error);
+        res.status(500).json({ error: 'Error al consultar la API externa' });
+    }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+}); 
